20f715692ccf5a0fe28730d61d73f3e7f38fa08a
AsitChandraRoy32 <gandharbikagiridhari2@gmail.com>
2024-07-07 00:26:34 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: "class",
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans", sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
