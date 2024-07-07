802694a9b8bd95f54212e5dcc1ad8fdf180053ec
AsitChandraRoy32 <gandharbikagiridhari2@gmail.com>
2024-07-07 22:21:52 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        javaScript: resolve(__dirname, "script.js"),
       },
     },
   },
